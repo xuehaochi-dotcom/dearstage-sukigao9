56e9c29dd15b6ffbaa5faf48c564c1c2024ccdfa
xuehaochi-dotcom <xue.haochi@gmail.com>
2026-09-21 14:07:00 +0900

    Update data.js

--- data.js
+++ data.js
@@ -2,301 +2,301 @@ const ARTISTS = [
   {
     "id": "a01",
     "name": "相沢梨紗",
-    "image": "images/a01.jpg"
+    "image": "images/a01.JPG"
   },
   {
     "id": "a02",
     "name": "相田詩音",
-    "image": "images/a02.jpg"
+    "image": "images/a02.JPG"
   },
   {
     "id": "a03",
     "name": "愛宮歌梨",
-    "image": "images/a03.jpg"
+    "image": "images/a03.JPG"
   },
   {
     "id": "a04",
     "name": "青柳透",
-    "image": "images/a04.jpg"
+    "image": "images/a04.JPG"
   },
   {
     "id": "a05",
     "name": "青山詩",
-    "image": "images/a05.jpg"
+    "image": "images/a05.JPG"
   },
   {
     "id": "a06",
     "name": "天音みほ",
-    "image": "images/a06.jpg"
+    "image": "images/a06.JPG"
   },
   {
     "id": "a07",
     "name": "天野ひかる",
-    "image": "images/a07.jpg"
+    "image": "images/a07.JPG"
   },
   {
     "id": "a08",
     "name": "綾瀬志希",
-    "image": "images/a08.jpg"
+    "image": "images/a08.JPG"
   },
   {
     "id": "a09",
     "name": "石浜芽衣",
-    "image": "images/a09.jpg"
+    "image": "images/a09.JPG"
   },
   {
     "id": "a10",
     "name": "一宮ゆい",
-    "image": "images/a10.jpg"
+    "image": "images/a10.JPG"
   },
   {
     "id": "a11",
     "name": "伊藤舞依",
-    "image": "images/a11.jpg"
+    "image": "images/a11.JPG"
   },
   {
     "id": "a12",
     "name": "宇田川ほむら",
-    "image": "images/a12.jpg"
+    "image": "images/a12.JPG"
   },
   {
     "id": "a13",
     "name": "岡田彩夢",
-    "image": "images/a13.jpg"
+    "image": "images/a13.JPG"
   },
   {
     "id": "a14",
     "name": "尾林結花",
-    "image": "images/a14.jpg"
+    "image": "images/a14.JPG"
   },
   {
     "id": "a15",
     "name": "佳神杏祇",
-    "image": "images/a15.jpg"
+    "image": "images/a15.JPG"
   },
   {
     "id": "a16",
     "name": "柏葉れん",
-    "image": "images/a16.jpg"
+    "image": "images/a16.JPG"
   },
   {
     "id": "a17",
     "name": "Kaya",
-    "image": "images/a17.jpg"
+    "image": "images/a17.JPG"
   },
   {
     "id": "a18",
     "name": "川端優",
-    "image": "images/a18.jpg"
+    "image": "images/a18.JPG"
   },
   {
     "id": "a19",
     "name": "隈本茉莉奈",
-    "image": "images/a19.jpg"
+    "image": "images/a19.JPG"
   },
   {
     "id": "a20",
     "name": "栗原舞優",
-    "image": "images/a20.jpg"
+    "image": "images/a20.JPG"
   },
   {
     "id": "a21",
     "name": "SAE",
-    "image": "images/a21.jpg"
+    "image": "images/a21.JPG"
   },
   {
     "id": "a22",
     "name": "桜野羽咲",
-    "image": "images/a22.jpg"
+    "image": "images/a22.JPG"
   },
   {
     "id": "a23",
     "name": "蔀祐佳",
-    "image": "images/a23.jpg"
+    "image": "images/a23.JPG"
   },
   {
     "id": "a24",
     "name": "詩之宮かこ",
-    "image": "images/a24.jpg"
+    "image": "images/a24.JPG"
   },
   {
     "id": "a25",
     "name": "島村嬉唄",
-    "image": "images/a25.jpg"
+    "image": "images/a25.JPG"
   },
   {
     "id": "a26",
     "name": "清水理子",
-    "image": "images/a26.jpg"
+    "image": "images/a26.JPG"
   },
   {
     "id": "a27",
     "name": "城谷美温",
-    "image": "images/a27.jpg"
+    "image": "images/a27.JPG"
   },
   {
     "id": "a28",
     "name": "白丸ゆん",
-    "image": "images/a28.jpg"
+    "image": "images/a28.JPG"
   },
   {
     "id": "a29",
     "name": "瀬﨑くるみ",
-    "image": "images/a29.jpg"
+    "image": "images/a29.JPG"
   },
   {
     "id": "a30",
     "name": "SOLI",
-    "image": "images/a30.jpg"
+    "image": "images/a30.JPG"
   },
   {
     "id": "a31",
     "name": "環やね",
-    "image": "images/a31.jpg"
+    "image": "images/a31.JPG"
   },
   {
     "id": "a32",
     "name": "チバゆな",
-    "image": "images/a32.jpg"
+    "image": "images/a32.JPG"
   },
   {
     "id": "a33",
     "name": "月雲ねる",
-    "image": "images/a33.jpg"
+    "image": "images/a33.JPG"
   },
   {
     "id": "a34",
     "name": "都月しより",
-    "image": "images/a34.jpg"
+    "image": "images/a34.JPG"
   },
   {
     "id": "a35",
     "name": "鶴見萌",
-    "image": "images/a35.jpg"
+    "image": "images/a35.JPG"
   },
   {
     "id": "a36",
     "name": "中村朱里",
-    "image": "images/a36.jpg"
+    "image": "images/a36.JPG"
   },
   {
     "id": "a37",
     "name": "逃げ水あむ",
-    "image": "images/a37.jpg"
+    "image": "images/a37.JPG"
   },
   {
     "id": "a38",
     "name": "根岸かのん",
-    "image": "images/a38.jpg"
+    "image": "images/a38.JPG"
   },
   {
     "id": "a39",
     "name": "Noa",
-    "image": "images/a39.jpg"
+    "image": "images/a39.JPG"
   },
   {
     "id": "a40",
     "name": "華丘琉愛",
-    "image": "images/a40.jpg"
+    "image": "images/a40.JPG"
   },
   {
     "id": "a41",
     "name": "花宮ハナ",
-    "image": "images/a41.jpg"
+    "image": "images/a41.JPG"
   },
   {
     "id": "a42",
     "name": "春乃好音",
-    "image": "images/a42.jpg"
+    "image": "images/a42.JPG"
   },
   {
     "id": "a43",
     "name": "日永陽咲",
-    "image": "images/a43.jpg"
+    "image": "images/a43.JPG"
   },
   {
     "id": "a44",
     "name": "蛭田愛梨",
-    "image": "images/a44.jpg"
+    "image": "images/a44.JPG"
   },
   {
     "id": "a45",
     "name": "広瀬みのり",
-    "image": "images/a45.jpg"
+    "image": "images/a45.JPG"
   },
   {
     "id": "a46",
     "name": "藤咲彩音",
-    "image": "images/a46.jpg"
+    "image": "images/a46.JPG"
   },
   {
     "id": "a47",
     "name": "古川未鈴",
-    "image": "images/a47.jpg"
+    "image": "images/a47.JPG"
   },
   {
     "id": "a48",
     "name": "前嶋杏乃",
-    "image": "images/a48.jpg"
+    "image": "images/a48.JPG"
   },
   {
     "id": "a49",
     "name": "的場華鈴",
-    "image": "images/a49.jpg"
+    "image": "images/a49.JPG"
   },
   {
     "id": "a50",
     "name": "澪田姫子",
-    "image": "images/a50.jpg"
+    "image": "images/a50.JPG"
   },
   {
     "id": "a51",
     "name": "湊音うの",
-    "image": "images/a51.jpg"
+    "image": "images/a51.JPG"
   },
   {
     "id": "a52",
     "name": "MINORI",
-    "image": "images/a52.jpg"
+    "image": "images/a52.JPG"
   },
   {
     "id": "a53",
     "name": "宮原梓",
-    "image": "images/a53.jpg"
+    "image": "images/a53.JPG"
   },
   {
     "id": "a54",
     "name": "MEW",
-    "image": "images/a54.jpg"
+    "image": "images/a54.JPG"
   },
   {
     "id": "a55",
     "name": "萌波あかり",
-    "image": "images/a55.jpg"
+    "image": "images/a55.JPG"
   },
   {
     "id": "a56",
     "name": "Mone",
-    "image": "images/a56.jpg"
+    "image": "images/a56.JPG"
   },
   {
     "id": "a57",
     "name": "桃谷まる",
-    "image": "images/a57.jpg"
+    "image": "images/a57.JPG"
   },
   {
     "id": "a58",
     "name": "桃ノ井理子",
-    "image": "images/a58.jpg"
+    "image": "images/a58.JPG"
   },
   {
     "id": "a59",
     "name": "八木遥叶",
-    "image": "images/a59.jpg"
+    "image": "images/a59.JPG"
   },
   {
     "id": "a60",
     "name": "吉乃櫻",
-    "image": "images/a60.jpg"
-  },
+    "image": "images/a60.JPG"
+  }
 ];
